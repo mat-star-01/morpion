093e704a140cd37a114808cb1d5f89d3cec5400f
mat-star-01 <mathis.i.miressi@gmail.com>
2020-05-23 12:34:01 +0200

    On a implemente: Morpion.afficher_la_grille Morpion.demander_au_joueur_ce_qu_il_veut_jouer Morpion.mettre_des_lettres_sur_la_case Morpion.changer_de_joueur Morpion.lui_dire_bravo

--- morpion.py
+++ morpion.py
@@ -1,36 +1,54 @@
 class Morpion:
     def __init__(self):
-        self.grille = "...\n...\n..."
-
+        self.grille = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
+        self.user_input = 0
+        self.tour_du_joueur = "X"
+        self.grille_dict = {
+            1: (2, 0),
+            2: (2, 1),
+            3: (2, 2),
+            4: (1, 0),
+            5: (1, 1),
+            6: (1, 2),
+            7: (0, 0),
+            8: (0, 1),
+            9: (0, 2),
+        }
 
     def afficher_la_grille(self):
-        print(self.grille)
-
+        print(" ".join(self.grille[0]))
+        print(" ".join(self.grille[1]))
+        print(" ".join(self.grille[2]))
 
     def demander_au_joueur_ce_qu_il_veut_jouer(self):
-        pass
-
+        self.user_input = int(input("Qu'est ce que tu veux jouer ?\n"))
+        # TODO: Verifier que self.user_input est valide.
+        #   ex invalide: 10
+        #   ex invalide: 0
+        #   ex invalide: -1
+        #   ex invalide: y
+        #   ex invalide: 102
+        #   ex invalide: 1000564
 
     def mettre_des_lettres_sur_la_case(self):
-        pass
-
+        coord = self.grille_dict[self.user_input]
+        self.grille[coord[0]][coord[1]] = self.tour_du_joueur
 
     def changer_de_joueur(self):
-        pass
-
+        if self.tour_du_joueur == "X":
+            self.tour_du_joueur = "O"
+        else:  # if self.tour_du_joueur == "O":
+            self.tour_du_joueur = "X"
 
     def verifier_si_il_a_gagne(self):
-        return True
-
+        return False
+        # TODO: a faire.
 
     def lui_dire_bravo(self):
-        pass
-
+        print("bravo")
 
     def jouer(self):
         while True:
-            a = "X"
-            z = "O"
             self.afficher_la_grille()
             self.demander_au_joueur_ce_qu_il_veut_jouer()
             self.mettre_des_lettres_sur_la_case()
